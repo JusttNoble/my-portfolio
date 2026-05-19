f4ddf6d4e2ba5e260877f907bb44e539f3e0ad98
JusttNoble <165186785+JusttNoble@users.noreply.github.com>
2026-05-19 14:31:03 -0700

    Add portfolio site

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (scrollY >= sectionTop - 100) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
+
+
+function sendEmail() {
+    const templateParams = {
+        from_name: document.getElementById("name").value,
+        from_email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send("service_3x1jn1q", "template_3b50iqe", templateParams)
+        .then(() => {
+            alert("Email sent successfully!");
+        }, (error) => {
+            console.error("Failed to send email:", error);
+        });
+}
